d9a14e5987feedefff8cbf580aa0ae0d39284907
baoph <baokhanh0514@gmail.com>
2024-02-27 14:30:47 -0700

    update

--- src/lab07/PoorRandomNumberGenerator.java
+++ src/lab07/PoorRandomNumberGenerator.java
@@ -1,12 +1,5 @@
 package lab07;
 
-@SuppressWarnings("unused")
-/**
- * Implementation of an RNG that produces a very non-random sequence of numbers.
- * 
- * @author CS 2420 instructors
- * @version July 17, 2023
- */
 public class PoorRandomNumberGenerator implements RandomNumberGenerator {
 
 	// NOTE: None of these variables are used in this generator.
