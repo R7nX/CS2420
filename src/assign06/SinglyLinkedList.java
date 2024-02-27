5542f76d00892e2b76104477d86d950ffc4ac8ad
baoph <baokhanh0514@gmail.com>
2024-02-27 15:40:37 -0700

    Implemented Iterator and private Node class

--- src/assign06/SinglyLinkedList.java
+++ src/assign06/SinglyLinkedList.java
@@ -3,78 +3,123 @@ package assign06;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class SinglyLinkedList<T> implements List<Object> {
+public class SinglyLinkedList<T> implements List<T> {
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node<T> head;
+    private int size;
+
+    public SinglyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    @Override
+    public void insertFirst(T element) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void insert(int index, T element) throws IndexOutOfBoundsException{
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public T getFirst() throws NoSuchElementException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T get(int index) throws IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T deleteFirst() throws NoSuchElementException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T delete(int index) throws IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int indexOf(T element) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
 	@Override
-	public void insertFirst(Object element) {
-		// TODO Auto-generated method stub
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	}
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public T[] toArray() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 	@Override
-	public void insert(int index, Object element) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Object getFirst() throws NoSuchElementException {
-		// TODO Auto-generated method stub
+	public Iterator<T> iterator() {
 		return null;
 	}
 
-	@Override
-	public Object get(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	private class SinglyListIterator implements Iterator<T> {
+        private Node<T> curr = head;
+        private Node<T> pre = null;
 
-	@Override
-	public Object deleteFirst() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        @Override
+        public boolean hasNext() {
+            return curr != null;
+        }
 
-	@Override
-	public Object delete(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T data = curr.data;
+            pre = curr;
+            curr = curr.next;
+            return data;
+        }
 
-	@Override
-	public int indexOf(Object element) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void clear() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Iterator iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        @Override
+        public void remove() {
+            if (pre == null) {
+                throw new NoSuchElementException();
+            }
+            pre.next = curr;
+        }
+    }
 
 }
