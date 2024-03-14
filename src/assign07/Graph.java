2de23fbf1baf64ff041fdc6e959c86b05511f9ec
baoph <baokhanh0514@gmail.com>
2024-03-13 21:30:23 -0600

    Finished bfs

--- src/assign07/Graph.java
+++ src/assign07/Graph.java
@@ -1,7 +1,8 @@
 package assign07;
 
-import java.util.HashMap;
-import java.util.List;
+import java.sql.Array;
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Graph<T> extends GraphUtility<T> {
     private final HashMap<T, Vertex<T>> vertices;
@@ -28,14 +29,55 @@ public class Graph<T> extends GraphUtility<T> {
         }
     }
 
+    private Vertex<T> findVertex (T data){
+        for (Vertex<T> vertex : vertices.values()){
+            if (vertex.equals(data))
+                return vertex;
+        }
+        throw new IllegalArgumentException("Vertex with data " + data + " not found");
+    }
+
     public boolean dfs(T srcData, T dstData) {
         //TODO: Finish this fking method
         return false;
     }
 
     public List<T> bfs(T srcData, T dstData) {
-        //TODO: Write this shit
-        return null;
+       Vertex<T> srcVertex = findVertex(srcData);
+       Vertex<T> dstVertex = findVertex(dstData);
+
+        Queue<Vertex<T>> queue = new LinkedList<>();
+        Set<Vertex<T>> visited = new HashSet<>();
+
+        Map<Vertex<T>, Vertex<T>> parentMap = new HashMap<>();
+        List<T> path = new ArrayList<>();
+
+        queue.offer(srcVertex);
+        visited.add(srcVertex);
+
+        while (!queue.isEmpty()) {
+            Vertex<T> current = queue.poll();
+
+            while (current.edges().hasNext()) {
+                Edge<T> curr = current.edges().next();
+                Vertex<T> neighbor = curr.destination;
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    parentMap.put(neighbor, current);
+                    queue.offer(neighbor);
+                }
+            }
+
+            if (current == dstVertex){
+                Vertex<T> node = current;
+                while (node != null){
+                    path.add(0, node.getName());
+                    node = parentMap.get(node);
+                }
+                return path;
+            }
+        }
+        throw new IllegalArgumentException("No path found");
     }
 
     public List<T> topologicalSort() {
